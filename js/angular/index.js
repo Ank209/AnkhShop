a63f2e87d5509c1d137799e789789d25b193db64
Alex Till <aptill@our.ecu.edu.au>
2016-11-22 15:19:59 +0800

    no message

--- js/angular/index.js
+++ js/angular/index.js
@@ -1,48 +1,10 @@
-var app = angular.module('IndexApp', []);
+var app = angular.module('IndexApp', ['breeze.angular']);
+
+app.run(['breeze', function (breeze) { }]);
 
 app.controller('IndexCtrl', function($scope) {
     $scope.login = function() {
 		//Check user and pass against database
-		var Connection = require('tedious').Connection;  
-		var config = {  
-			userName: 'Ankh',  
-			password: 'SmashingPineapples1',  
-			server: 'ankhdata.database.windows.net',  
-			// If you are on Microsoft Azure, you need this:  
-			options: {encrypt: true, database: 'ShopDataFree'}  
-		};  
-		var connection = new Connection(config);  
-		connection.on('connect', function(err) {  
-		// If no error, then good to proceed.  
-			console.log("Connected");  
-		}); 
-		
-		var Request = require('tedious').Request;  
-		var TYPES = require('tedious').TYPES;  
-	  
-		function executeStatement() {  
-			request = new Request("SELECT Pass FROM Account WHERE UserName = " + $scope.userName + ";", function(err) {  
-			if (err) {  
-				console.log(err);}  
-			});  
-			var result = "";  
-			request.on('row', function(columns) {  
-				columns.forEach(function(column) {  
-				  if (column.value === null) {  
-					console.log('NULL');  
-				  } else {  
-					result+= column.value + " ";  
-				  }  
-				});  
-				console.log(result);  
-				result ="";  
-			});  
-	  
-			request.on('done', function(rowCount, more) {  
-			console.log(rowCount + ' rows returned');  
-			});  
-			connection.execSql(request);  
-		}
 		
 	}
 });
